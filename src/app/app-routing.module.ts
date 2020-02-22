2c45988f690dd1ec27405f214a265abfa35d26bd
OlehBiloholovka <35770198+OlehBiloholovka@users.noreply.github.com>
2020-02-22 19:10:40 +0200

    added routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+import {CoreComponent} from './core/core/core.component';
 
-
+export const routes: Routes = [
+  {
+    path: '',
+    component: CoreComponent,
+  },
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class AppRoutingModule { }
